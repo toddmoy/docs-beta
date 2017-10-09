68049f98375f1d27021fe87c3e22ec0cf53a9feb
Todd Moy <todd.moy@gmail.com>
2017-10-09 14:21:18 -0600

    Add links to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
+import Link from 'gatsby-link'
 
 const Header = () => (
-  <header>Header</header>
+  <header>
+    <a href="https://sendgrid.com">SendGrid</a>
+    <ul>
+      <li><Link to="/">Help &amp; Support</Link></li>
+      <li><Link to="/for-developers">For Developers</Link></li>
+      <li><Link to="/glossary">Glossary</Link></li>
+      <li><a href="http://status.sendgrid.com/">Status</a></li>
+    </ul>
+  </header>
 )
 
 export default Header
