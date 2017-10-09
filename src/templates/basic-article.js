8b7cd439e491f8fac64ac153956f34a47046b4d1
Todd Moy <todd.moy@gmail.com>
2017-10-09 14:22:20 -0600

    Add alert

--- src/templates/basic-article.js
+++ src/templates/basic-article.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import Alert from '../components/Alert'
 
 export default function Template ({data}) {
   const { markdownRemark: post } = data
@@ -9,6 +10,7 @@ export default function Template ({data}) {
   return (
     <div>
       <Helmet title={ `${post.frontmatter.title} - SendGrid` } />
+      <Alert message="SendGrid is experiencing technical difficulties." />
       <Header />
       <div>
         <h1>{post.frontmatter.title}</h1>
