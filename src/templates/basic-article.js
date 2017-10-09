1fd09b08fc2cc54ba85c0799618f0909e09eaa92
Todd Moy <todd.moy@gmail.com>
2017-10-09 13:57:43 -0600

    Add Header and Footer.

--- src/templates/basic-article.js
+++ src/templates/basic-article.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Helmet from 'react-helmet'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 export default function Template ({data}) {
   const { markdownRemark: post } = data
@@ -7,10 +9,12 @@ export default function Template ({data}) {
   return (
     <div>
       <Helmet title={ `${post.frontmatter.title} - SendGrid` } />
+      <Header />
       <div>
         <h1>{post.frontmatter.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
       </div>
+      <Footer />
     </div>
   )
 }
